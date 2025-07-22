8677889f9ff99b8f730cf2c47ae44b1692c4255d
moran <sharon.ky.moran@gmail.com>
2025-07-22 13:41:57 -0400

    define what lifting state means

--- react-notes/src/components/LearnLiftingStateUp.jsx
+++ react-notes/src/components/LearnLiftingStateUp.jsx
@@ -4,6 +4,14 @@ const LearnLiftingStateUp = () => {
   return (
     <>
       <h2>Lifting State Up: Data from Child to Parent</h2>
+
+      <p>
+        "Lifting state up" is a design pattern in component-based UI frameworks
+        like React and Flutter, used to manage and share state between multiple
+        components. It involves moving the state that is shared or manipulated
+        by several child components to their closest common ancestor (parent
+        component).
+      </p>
     </>
   );
 };
