d4a42726503adb68948c40612a6a7af8a2ec6f1a
moran <sharon.ky.moran@gmail.com>
2025-07-22 00:49:13 -0400

    creation and utilization of a simple component

--- react-notes/src/App.jsx
+++ react-notes/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 
+// add Hellow World Component via import
+import HelloWorld from "./components/HelloWorld";
+
 function App() {
   return (
     <>
       <h1>React Notes</h1>
+      {/* add the Hello World component to utilize it in the app */}
+      <HelloWorld />
     </>
   );
 }
---
+++ react-notes/src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+// arror function creates the component
+const HelloWorld = () => {
+  // return jsx
+  return <h2>Hello World!</h2>;
+};
+
+// to use a component you need to export it here and then import it in the file you want to use it in
+export default HelloWorld;
