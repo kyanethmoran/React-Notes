d27d05d48c904cded61c6cb8261497730ef9723e
moran <sharon.ky.moran@gmail.com>
2025-07-24 15:49:59 -0400

    init conditional rendering component

---
+++ react-notes/src/components/LearnConditionalRendering.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LearnConditionalRendering = () => {
+  return (
+    <>
+      <h2>Conditional Rendering in React:</h2>
+      <h3>What is it?</h3>
+      <p>inset text here</p>
+    </>
+  );
+};
+
+export default LearnConditionalRendering;
