e2544e2111695a4cb3f3ff6f98b8648652e21a8b
moran <sharon.ky.moran@gmail.com>
2025-07-24 13:16:30 -0400

    prop dilling and context api done

--- react-notes/src/components/LearnUseContext.jsx
+++ react-notes/src/components/LearnUseContext.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { createContext } from "react";
 import ChildA from "../helperComponents/childA";
+
+//  #1 for use with context API create the context
+export const contextExample = createContext();
 
 const LearnUseContext = () => {
   // this is the data i will be prop drilling from here to ChildC
-  const text = "PropDrilled from the parent";
+  const text = `"This string was prop drilled from the parent!"`;
+
+  //   data for the context api to provide and consume, can be used with hook or contextName.comsumer
+  const contextData = "This string came from using context";
   return (
     <>
       <h2>useContext Hook:</h2>
@@ -34,11 +40,59 @@ const LearnUseContext = () => {
           application, regardless of its nesting depth.
         </li>
       </ol>
+      <h3>Prop Drilling:</h3>
+      <label htmlFor="drilling-order">
+        The data gets explicitly pass from component to component with no skips
+        in components.
+      </label>
+      <ol id="drilling-order">
+        <li>LearnUseContext: data created here</li>
+        <li>ChildA: passes through</li>
+        <li>ChildB: passes through</li>
+        <li>ChildC: get here and is displayed</li>
+      </ol>
       <p>
         If we have data in this component that we want to get to childC it needs
         to pass through childA then childB first
       </p>
-      <ChildA text={text} />
+      <h3>Context API:</h3>
+      <label htmlFor="three-context-api">
+        When working with context API you need to keep three things in mind:
+      </label>
+      <ol>
+        <li>
+          <strong>Create: </strong> you need to create the context first
+        </li>
+        <li>
+          <strong>Provide: </strong> The Context.Provider component is used to
+          make the context value available to its children. Wrap the component
+          tree that needs access to the context with the Provider. Pass the
+          context value using the value prop.
+          <ul>
+            <li>The provider is used in both context API and useContext</li>
+          </ul>
+        </li>
+        <li>
+          <strong>Consumer: </strong> Use the useContext hook (for functional
+          components) or the Context.Consumer component (for class components)
+          to access the context value.
+          <ul>
+            <li>
+              With context API we need to consume the context with
+              <code> &lt;Context.Consumer&gt;</code>
+            </li>
+          </ul>
+        </li>
+      </ol>
+      <h3>Lets see how they all work:</h3>
+      <ul>
+        <li>
+          {/* wrap the component tree that needs access to the context*/}
+          <contextExample.Provider value={contextData}>
+            <ChildA text={text} />
+          </contextExample.Provider>
+        </li>
+      </ul>
     </>
   );
 };
--- react-notes/src/helperComponents/childC.jsx
+++ react-notes/src/helperComponents/childC.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+// import context from parent component
+import { contextExample } from "../components/LearnUseContext";
 
 const ChildC = (props) => {
+  // consume the data from the context to acces context value
+  const contextData = useContext(contextExample);
   return (
     <>
+      <div>ChildC : </div>
       <div>
-        ChildC : here is the data prop-drilled from 3 components up -&gt;
-        <br />
-        <strong>{props.text}</strong>
+        <ul>
+          {/* this version uses just prop drilled from parent explicitly through all the children */}
+          <li>PropDrilled Data: {props.text}</li>
+
+          {/* This version uses just context api's consumer no hook to pass the data */}
+          <contextExample.Consumer>
+            {(value) => <li>Context API Data: {value}</li>}
+          </contextExample.Consumer>
+
+          {/* this version uses the useContext hook which removes the need to specify the consumer and value */}
+          <li>UseContext Data: {contextData}</li>
+        </ul>
       </div>
     </>
   );
