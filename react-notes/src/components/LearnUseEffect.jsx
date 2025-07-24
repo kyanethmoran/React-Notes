357a6bdd15a600173fd9b3f0d87c777bcc7dbd55
moran <sharon.ky.moran@gmail.com>
2025-07-23 22:27:22 -0400

    useEffect notes and example complete

--- react-notes/src/components/LearnUseEffect.jsx
+++ react-notes/src/components/LearnUseEffect.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react";
 
 const LearnUseEffect = () => {
+  // state variable for count to use with the countdown button
   const [count, setCount] = useState(100);
+  const [isCounting, setIsCounting] = useState(false);
 
-  const startTimer = () => {};
+  // utilizing useEffect to carry out the down down
+  useEffect(() => {
+    if (!isCounting || count == 0) return;
+
+    const countdown = setTimeout(() => {
+      setCount((prev) => prev - 1);
+    }, 1000);
+
+    // we want to clear the timeout so we can rerun the effect and prevent overlapping timers from repeat renders
+    return () => clearTimeout(countdown);
+  }, [isCounting, count]);
+
+  //  this function will trigger the use effect and start the timer
+  const startTimer = () => {
+    setIsCounting(true);
+  };
+
+  //   this function is just a simple reset to make the start timer button a repeatable action
+  const resetTimer = () => {
+    setIsCounting(false);
+    setCount(100);
+  };
+
   return (
     <>
-      <h2>use Effect Hook</h2>
+      <h2>useEffect Hook</h2>
       <h3>What is it?</h3>
       <p>
         useEffect is a React Hook that lets you run side effects in functional
@@ -20,11 +44,11 @@ const LearnUseEffect = () => {
             <code>
               {`import { useEffect } from 'react';
 
-useEffect(() => {
+ useEffect(() => {
     setTimeout(() => {
       setCount((count) => count + 1);
     }, 1000);
-});`}
+ });`}
             </code>
           </pre>
           <ul>
@@ -33,6 +57,9 @@ useEffect(() => {
               <br />
               <button onClick={startTimer}>Start Timer</button>
               Current count is: {count}
+              <br />
+              <button onClick={resetTimer}>Reset Timer</button>
+              Stop and reset the Count
             </li>
           </ul>
         </li>
