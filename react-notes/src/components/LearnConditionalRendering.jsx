d8724e4d64d87a6c71bb10247a1afeee75c5148a
moran <sharon.ky.moran@gmail.com>
2025-07-24 15:55:46 -0400

    define and explain conditional rendering

--- react-notes/src/components/LearnConditionalRendering.jsx
+++ react-notes/src/components/LearnConditionalRendering.jsx
@@ -5,7 +5,53 @@ const LearnConditionalRendering = () => {
     <>
       <h2>Conditional Rendering in React:</h2>
       <h3>What is it?</h3>
-      <p>inset text here</p>
+      <p>
+        Conditional Rednering allows us to dynamically show or hide elements or
+        change the way that a component renders based on different criteria that
+        we set. This is important becuase we can use state to identify what the
+        user may need to see or be able to access or interact with.
+      </p>
+      <h3>Key Concepts: </h3>
+      <ul>
+        <li>
+          <strong>Dynamic UI:</strong> Conditional rendering enables the UI to
+          change and adapt based on data and user interactions.
+        </li>
+        <li>
+          <strong>State Management:</strong> Often, conditional rendering is
+          used in conjunction with state management to update the UI based on
+          changes in the application's state.
+        </li>
+        <li>
+          <strong>Component Re-use:</strong> Instead of creating separate
+          components for every possible state, conditional rendering allows you
+          to reuse components and adjust their content or appearance based on
+          conditions.
+        </li>
+      </ul>
+      <h3>Common Techniques: </h3>
+      <ul>
+        <li>
+          if/else statements: Suitable for simple scenarios with a few
+          conditional branches.
+        </li>
+        <li>
+          Ternary operator (? :): Provides a concise way to handle two-way
+          conditional rendering.
+        </li>
+        <li>
+          Logical AND (&&): Efficient for conditionally rendering a component
+          only when a condition is true.
+        </li>
+        <li>
+          switch statements: Useful for handling multiple conditional branches
+          based on different values.
+        </li>
+        <li>
+          Element variables: Assigning components to variables based on
+          conditions and then rendering the variable.{" "}
+        </li>
+      </ul>
     </>
   );
 };
